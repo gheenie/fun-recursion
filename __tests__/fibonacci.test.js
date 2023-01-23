decc9fc50a3d1089220f2b8f2cfe5fb027789b70
infoomics <sbx_94@hotmail.com>
2023-01-23 18:04:47 +0000

    renamed files and refactored

--- __tests__/fibonacci.test.js
+++ __tests__/fibonacci.test.js
@@ -1,14 +1,15 @@
-const fibonacci = require('../katas/fibonacci');
+const fibonacci = require('../katas/6-fibonacci');
 
 describe('fibonacci', () => {
     test('returns a number', () => {
         const input = 1;
 
-        const output = fibonacci(input);
+        const output = typeof fibonacci(input);
 
         const expected = 'number';
-        expect(typeof output).toBe(expected);
+        expect(output).toBe(expected);
     });
+    
     test('returns the number in the fobonacci sequence', () => {
         const input = 7;
 
