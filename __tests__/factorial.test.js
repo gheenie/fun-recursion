988ce1a25e7aae71c24bb337ad355b051fd339c3
infoomics <sbx_94@hotmail.com>
2023-01-23 17:45:44 +0000

    added more robust tests

--- __tests__/factorial.test.js
+++ __tests__/factorial.test.js
@@ -1,16 +1,34 @@
 const factorial = require('../katas/1-factorial');
 
-describe('', () => {
+describe('happy paths', () => {
     test('return type of output', () => {
         const input = 4;
 
-        const output = factorial(input);
+        const output = typeof factorial(input);
 
         const expected = 'number';
-        expect(typeof output).toBe(expected);
+        expect(output).toBe(expected);
     });
 
-    test('final value of output', () => {
+    test('special input 0', () => {
+        const input = 0;
+
+        const output = factorial(input);
+
+        const expected = 1;
+        expect(output).toBe(expected);
+    });
+
+    test('special input 1', () => {
+        const input = 1;
+
+        const output = factorial(input);
+
+        const expected = 1;
+        expect(output).toBe(expected);
+    });
+
+    test('input is above 1', () => {
         const input = 4;
 
         const output = factorial(input);
