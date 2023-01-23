05d463aad18b7770707a9e1627ed590351cb56bf
infoomics <sbx_94@hotmail.com>
2023-01-23 18:03:44 +0000

    renamed files and refactored

--- __tests__/sum-digits.test.js
+++ __tests__/sum-digits.test.js
@@ -1,38 +1,39 @@
-const sumDigits = require('../katas/sum-digits');
+const sumDigits = require('../katas/5-sum-digits');
 
-describe('sumDigits', () => {
+describe('happy paths', () => {
     test('returns a number', () => {
         const input = 0;
 
         const output = sumDigits(input);
 
-        const expected = 'number';
-        expect(typeof output).toBe(expected);
+        const expected = typeof 'number';
+        expect(output).toBe(expected);
     });
+
     test('only one digit returns the same digit', () => {
         const input = 1;
 
         const output = sumDigits(input);
 
-        expect(output).toEqual(input);
+        const expected = input;
+        expect(output).toEqual(expected);
     });
-    test('multiple digits returns the sum of its digits', () => {
+
+    test('multiple digits returns the sum of its digits; 2 calls', () => {
         const input = 999;
 
         const output = sumDigits(input);
 
         const expected = 9;
-
         expect(output).toBe(expected);
     });
-    test('multiple digits returns the sum of its digits', () => {
+
+    test('multiple digits returns the sum of its digits; 3 calls', () => {
         const input = 88888888;
 
         const output = sumDigits(input);
 
         const expected = 1;
-
         expect(output).toBe(expected);
     });
 });
-;
