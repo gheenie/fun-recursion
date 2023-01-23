17dada14ed049009e645be9cab8acfbd305bc8bb
infoomics <sbx_94@hotmail.com>
2023-01-23 17:13:14 +0000

    added tests for array

--- __tests__/stringify.test.js
+++ __tests__/stringify.test.js
@@ -10,7 +10,7 @@ describe('stringify()', () => {
         expect(output).toBe(expected);
     });
 
-    test('one level object only; has whitespaces', () => {
+    test('one level object; has whitespaces', () => {
         const input = { x: 5, y: 6 };
 
         const output = stringify(input);
@@ -19,12 +19,30 @@ describe('stringify()', () => {
         expect(output).toBe(expected);
     });
 
-    test('deep level object only; has whitespaces', () => {
+    test('deep level object; has whitespaces', () => {
         const input = { x: 5, y: { a: 1, b: 2 } };
 
         const output = stringify(input);
 
         const expected = '{"x":5,"y":{"a":1,"b":2}}';
+        expect(output).toBe(expected);
+    });
+
+    test('empty array', () => {
+        const input = [];
+
+        const output = stringify(input);
+
+        const expected = '[]';
+        expect(output).toBe(expected);
+    });
+
+    test('one level array; different data types; has whitespaces', () => {
+        const input = [new Number(3), new String('false'), new Boolean(false)];
+
+        const output = stringify(input);
+
+        const expected = '[3,"false",false]';
         expect(output).toBe(expected);
     });
 });
