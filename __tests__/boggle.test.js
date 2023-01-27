a26521b060bf2c914c41dcf6b49d4e4cdf2e4727
infoomics <sbx_94@hotmail.com>
2023-01-27 03:07:32 +0000

    rearranged tests

--- __tests__/boggle.test.js
+++ __tests__/boggle.test.js
@@ -1,24 +1,30 @@
 const boggle = require('../katas/16-boggle');
 
 describe('', () => {
-    test('', () => {
-        const board = [
-            ["e", "a", "r", "a"],
-            ["n", "l", "e", "c"],
-            ["i", "a", "i", "s"],
-            ["b", "y", "o", "r"],
-        ];
+    const board = [
+        ["e", "a", "r", "a"],
+        ["n", "l", "e", "c"],
+        ["i", "a", "i", "s"],
+        ["b", "y", "o", "r"],
+    ];
 
-        /*// Letter not in board
+    test('letter not in board', () => {
         expect( boggle(board, "z") ).toBe(false);
-        // One letter, roughly mid of board
+    });
+
+    test('one letter match, roughly mid of board', () => {
         expect( boggle(board, "l") ).toBe(true);
-        // 2 letters
+    });
+    
+    test('two letter match', () => {
         expect( boggle(board, "lr") ).toBe(true);
+    });
+
+    test.only('other tests', () => {
         expect( boggle(board, "ear") ).toBe(true); // true
-        expect( boggle(board, "ears") ).toBe(false); // false*/
+        /*expect( boggle(board, "ears") ).toBe(false); // false
         expect( boggle(board, "rio") ).toBe(true); // true
-        /*expect( boggle(board, "cereal") ).toBe(false); // false
+        expect( boggle(board, "cereal") ).toBe(false); // false
         expect( boggle(board, "rscareioybailnea") ).toBe(true); // true*/
     });
 });
